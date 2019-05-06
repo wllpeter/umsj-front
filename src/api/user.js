8a4724e4c5ada9384599488312acbdd9c2d0cbce
weiliangliang <weiliangliang@tuniu.com>
2019-05-06 10:38:39 +0800

    init

---
+++ src/api/user.js
@@ -0,0 +1,25 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: '/api/user/login',
+    method: 'post',
+    data
+  })
+}
+
+export function getInfo() {
+  return request({
+    url: '/api/user/info',
+    method: 'get',
+    params: {}
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/api/user/logout',
+    method: 'post'
+  })
+}
+
---
+++ src/views/permission/user.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app-container">
+    <h3>用户管理</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserMangement'
+}
+</script>
